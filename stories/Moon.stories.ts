9472d1aecb84e3d9db84641fec0fb0a99ff20357
Aaron Blum <aaron@localyzeapp.com>
2023-12-10 23:24:10 +0100

    added storybook and moon

---
+++ stories/Moon.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Moon from "../app/components/Moon/Moon";
+
+const meta = {
+  title: "Example/Moon",
+  component: Moon,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Moon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {
+    primary: true,
+    label: "Moon",
+  },
+};
