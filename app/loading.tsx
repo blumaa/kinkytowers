62e1a173e98e0ee369f2f15803b796d2f3dd99ba
Aaron Blum <blumaa@gmail.com>
2023-12-11 22:36:20 +0100

    add towerscene

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center bg-sky-900 h-screen">
+    <div className="flex items-center justify-center bg-sky-900 h-screen w-full">
       coming soon...
     </div>
   );
