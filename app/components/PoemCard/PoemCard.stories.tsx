d818c3bf6a103455b100dd64e41bf436b317a8d4
Aaron Blum <aaron@localyzeapp.com>
2023-12-13 23:41:16 +0100

    layout adjusted

--- app/components/PoemCard/PoemCard.stories.tsx
+++ app/components/PoemCard/PoemCard.stories.tsx
@@ -21,12 +21,14 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {
+    title: "poem",
     children: "PoemCard",
   },
 };
 
 export const WithLongText: Story = {
   args: {
+    title: "Moon Poem #156",
     children: moonPoem,
   },
 };
